351dbf9c4c03a055bc2623e361ad6edd8f9117b0
jhamalmckee <92033046+jhamalmckee@users.noreply.github.com>
2022-12-01 15:55:44 -0500

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -129,7 +129,7 @@ resource "aws_security_group" "loadbalancer-sg" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
-    security_groups = [aws_security_group.web_sg.id]
+    security_groups = [aws_security_group.week24_web_sg.id]
   }
 
   egress {
